52f1225fd6935e7fe3d7804d4fa0071c48a9343a
techwithken <ti664469@gmail.com>
2024-06-15 01:20:31 +0100

    Added a templates to add the html file to the django application

--- cardapp/views.py
+++ cardapp/views.py
@@ -4,9 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("This the the Home for the Card")
+    return render(request, "cardapp/index.html")
 
 def allcards(request, name):
-    return HttpResponse(f"This is the {name} color card for the Application")
+    return HttpResponse(f"This is the {name.upper()} color card for the Application")
 
 
